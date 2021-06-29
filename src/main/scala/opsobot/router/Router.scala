26d44d5cb271819492fb96aafa2b35c5d1895030
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-06-29 10:50:39 +0200

    changed port

--- src/main/scala/opsobot/router/Router.scala
+++ src/main/scala/opsobot/router/Router.scala
@@ -22,7 +22,7 @@ class Router {
         }
       }
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("localhost", 80).bind(route)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
