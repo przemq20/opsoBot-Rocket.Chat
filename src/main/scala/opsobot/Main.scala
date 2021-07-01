75f69e1aceccc62547a3f4aa09222ac2e7462e12
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-07-01 14:33:30 +0200

    2

--- src/main/scala/opsobot/Main.scala
+++ src/main/scala/opsobot/Main.scala
@@ -7,12 +7,12 @@ import akka.http.scaladsl.server.Directives._
 
 object Main {
   def main(args: Array[String]):Unit = {
-    //    new Thread{
-    //      override def run(): Unit = {
-    //        val streamBot = new StreamBot
-    //        streamBot.run()
-    //      }
-    //    }.start()
+        new Thread{
+          override def run(): Unit = {
+            val streamBot = new StreamBot
+            streamBot.run()
+          }
+        }.start()
     //    new Thread{
     //      override def run(): Unit = new Router().run()
     //    }.start()
