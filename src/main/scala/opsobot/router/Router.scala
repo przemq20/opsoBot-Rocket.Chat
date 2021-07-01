e5bf201ca1111843c95ea5d08d76ded927859ce4
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-07-01 14:24:01 +0200

    1

--- src/main/scala/opsobot/router/Router.scala
+++ src/main/scala/opsobot/router/Router.scala
@@ -16,12 +16,13 @@ class Router {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.executionContext
 
-    val route =
-      path("") {
+    val route = {
+      pathEndOrSingleSlash {
         get {
           complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
         }
       }
+    }
     val host = "0.0.0.0"
     val port: Int = sys.env.getOrElse("PORT", "8080").toInt
 
@@ -29,11 +30,10 @@ class Router {
     val bindingFuture = Http().newServerAt(host, port).bind(route)
     println(System.getenv("SERVER_URL"))
     println(System.getenv("PORT"))
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    println(s"Server online at $host:$port\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
 }
-
