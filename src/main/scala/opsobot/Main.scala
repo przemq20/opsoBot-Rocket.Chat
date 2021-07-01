cdb284b7075a1f60c2c9b82d8516f2e6d06b393a
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-07-01 14:30:57 +0200

    If this work...

--- src/main/scala/opsobot/Main.scala
+++ src/main/scala/opsobot/Main.scala
@@ -1,18 +1,35 @@
 package opsobot
 
-import opsobot.router.Router
-import opsobot.streambot.StreamBot
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.server.Directives._
 
 object Main {
-  def main(args: Array[String]): Unit = {
-    new Thread{
-      override def run(): Unit = {
-        val streamBot = new StreamBot
-        streamBot.run()
-      }
-    }.start()
-    new Thread{
-      override def run(): Unit = new Router().run()
-    }.start()
+  def main(args: Array[String]):Unit = {
+    //    new Thread{
+    //      override def run(): Unit = {
+    //        val streamBot = new StreamBot
+    //        streamBot.run()
+    //      }
+    //    }.start()
+    //    new Thread{
+    //      override def run(): Unit = new Router().run()
+    //    }.start()
+
+    val route = get {
+      complete(
+        HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          "hello"
+        )
+      )
+    }
+    implicit val system = ActorSystem("Server")
+
+    val host = "0.0.0.0"
+    val port: Int = sys.env.getOrElse("PORT", "8080").toInt
+
+    Http().bindAndHandle(route, host, port)
   }
 }
