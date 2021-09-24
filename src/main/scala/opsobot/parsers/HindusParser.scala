34db85ecffa9b1f4605ccb7144929f74a14208a9
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-09-24 08:43:28 +0200

    Removed 'extends App'

--- src/main/scala/opsobot/parsers/HindusParser.scala
+++ src/main/scala/opsobot/parsers/HindusParser.scala
@@ -6,22 +6,23 @@ import org.jsoup.select.Elements
 
 import javax.net.ssl.{ HostnameVerifier, SSLSession }
 
-object HindusParser extends App with Parser {
-  final val MENU_URL = "https://www.facebook.com/HindusFood-Krak%C3%B3w-MENU-110617490850262"
+object HindusParser extends Parser {
+  final val MENU_URL = "https://m.facebook.com/HindusFood-Krak%C3%B3w-MENU-110617490850262/"
 
   def parse(): Menu = {
     javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
       new HostnameVerifier {
         override def verify(hostname: String, session: SSLSession): Boolean =
-          hostname.equals("www.facebook.com"); // or return true
+          hostname.equals("m.facebook.com"); // or return true
       }
     )
 
     val document: Document = Jsoup.connect(MENU_URL).get()
-    println(document.select("rq0escxv l9j0dhe7 du4w35lb qmfd67dx hpfvmrgz gile2uim buofh1pr g5gj957u aov4n071 oi9244e8 bi6gxh9e h676nmdw aghb5jc5"))
+    println(document.select("#page"))
     val menu = new Menu()
 
     val dishTypeBlocks: Elements = document.select(".menu-category-block")
+//    println(dishTypeBlocks)
     if (dishTypeBlocks.isEmpty) {
       //      throw NoUpdatedMenuException("Olimp menu is unavailable")
       scribe.error("Olimp menu is unavailable")
