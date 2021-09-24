256028192500c164f020add6c3c05314d3527136
Przemysław Lechowicz <przemyslaw.lechowicz@comarch.com>
2021-09-24 12:58:02 +0200

    hindus menu

--- src/main/scala/opsobot/parsers/FoodTrucksParser.scala
+++ src/main/scala/opsobot/parsers/FoodTrucksParser.scala
@@ -4,18 +4,39 @@ import java.util.{ Calendar, TimeZone }
 
 object FoodTrucksParser extends Parser {
   def parse(): Menu = {
-    val category = "Dzisiejsze Food Trucki"
-    val menu     = new Menu
-    menu.addCategory(category)
-    val todayFoodTrucks = Calendar.getInstance(TimeZone.getTimeZone("GMT+2")).get(Calendar.DAY_OF_WEEK) match {
-      case Calendar.MONDAY    => List("RAJSKIE TAJSKIE - Kuchnia tajska", "GRILL MOBIL- Kuchnia turecko-grecka")
-      case Calendar.TUESDAY   => List("HINDUS INDIAN FOOD - kuchnia indyjska")
-      case Calendar.WEDNESDAY => List("MANUFAKTURA KRAKOWSKA - burgery i frytki", "PIEROGOWE LOVE - pierogi")
-      case Calendar.THURSDAY  => List("ROZBRYKANA OWCA - dania inspirowane kuchnią marokańską")
-      case Calendar.FRIDAY    => List("COMPADRE - kuchnia meksykańska", "CHINA BAR - kuchnia wietnamska")
-      case _                  => List("")
-    }
-    menu.addToCategory(category, todayFoodTrucks)
+    val menu = new Menu
+    val todayFoodTrucks: List[(String, List[String])] =
+      Calendar.getInstance(TimeZone.getTimeZone("GMT+2")).get(Calendar.DAY_OF_WEEK) match {
+        case Calendar.MONDAY =>
+          List(
+            ("RAJSKIE TAJSKIE - Kuchnia tajska", List.empty),
+            ("GRILL MOBIL- Kuchnia turecko-grecka", List.empty)
+          )
+        case Calendar.TUESDAY =>
+          List(
+            (HindusParser.foodTruckName, HindusParser.parse().dishes(HindusParser.category))
+          )
+        case Calendar.WEDNESDAY =>
+          List(
+            ("MANUFAKTURA KRAKOWSKA - burgery i frytki", List.empty),
+            ("Pizza - kuchnia włoska", List.empty)
+          )
+        case Calendar.THURSDAY =>
+          List(
+            ("ROZBRYKANA OWCA - dania inspirowane kuchnią marokańską", List.empty),
+            ("PIEROGOWE LOVE - pierogi", List.empty)
+          )
+        case Calendar.FRIDAY =>
+          List(
+            ("COMPADRE - kuchnia meksykańska", List.empty),
+            ("CHINA BAR - kuchnia wietnamska", List.empty)
+          )
+        case _ => List(("", List("")))
+      }
+    todayFoodTrucks.foreach(food => {
+      menu.addCategory(food._1)
+      menu.addToCategory(food._1, food._2)
+    })
     menu
   }
 }
--- src/main/scala/opsobot/parsers/HindusParser.scala
+++ src/main/scala/opsobot/parsers/HindusParser.scala
@@ -1,53 +1,33 @@
 package opsobot.parsers
 
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.select.Elements
-
-import javax.net.ssl.{ HostnameVerifier, SSLSession }
+import play.api.libs.json.Json
 
 object HindusParser extends Parser {
-  final val MENU_URL = "https://m.facebook.com/HindusFood-Krak%C3%B3w-MENU-110617490850262/"
+  final val MENU_URL = "https://hindus-api.herokuapp.com/hindus"
+  val foodTruckName = "HINDUS INDIAN FOOD - kuchnia indyjska"
+  val category      = ""
 
   def parse(): Menu = {
-    javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
-      new HostnameVerifier {
-        override def verify(hostname: String, session: SSLSession): Boolean =
-          hostname.equals("m.facebook.com"); // or return true
-      }
-    )
-
-    val document: Document = Jsoup.connect(MENU_URL).get()
-    println(document.select("#page"))
     val menu = new Menu()
 
-    val dishTypeBlocks: Elements = document.select(".menu-category-block")
-//    println(dishTypeBlocks)
-    if (dishTypeBlocks.isEmpty) {
-      //      throw NoUpdatedMenuException("Olimp menu is unavailable")
-      scribe.error("Olimp menu is unavailable")
+    val connection = Jsoup
+      .connect(MENU_URL)
+      .timeout(100000)
+      .ignoreHttpErrors(true)
+      .ignoreContentType(true)
+      .execute()
+    val code = connection.statusCode()
+    if (code != 200) {
+      menu.addCategory(category, List.empty)
       return menu
     }
-    dishTypeBlocks.forEach(block => {
-      val dishType = block.select("h3").text
+    val document = connection.parse()
 
-      val spanTagRegex = "(.*)<span>(.*)</span>".r
-      val parsedDishType = dishType match {
-        case spanTagRegex(a, b) => a + b
-        case _                  => dishType
-      }
+    val json = Json.parse(document.body().text())
 
-      menu.addCategory(parsedDishType)
-
-      val dishes = block
-        .select(".menu-dishes")
-        .text()
-        .split(',')
-        .map(_.trim)
-        .toList
-
-      menu.addToCategory(parsedDishType, dishes)
-    })
+    val list = json.as[List[String]]
+    menu.addCategory(category, list)
     menu
   }
   println(parse())
