b43b42f39738dc57159063a997f11b9bfd3443fd
przemq20 <lechowiczprzemyslaw@gmail.com>
2022-10-13 10:26:54 +0200

    Updated foodtrucks and makarun menu

--- src/main/scala/opsobot/parsers/MakarunParser.scala
+++ src/main/scala/opsobot/parsers/MakarunParser.scala
@@ -8,11 +8,11 @@ object MakarunParser extends Parser {
     val menu     = new Menu
     menu.addCategory(category)
     val todayPromo = Calendar.getInstance(TimeZone.getTimeZone("GMT+2")).get(Calendar.DAY_OF_WEEK) match {
-      case Calendar.MONDAY    => "Klasyczne poniedziałki - duży makaron Bolognese lub Carbonara w cenie małego."
-      case Calendar.TUESDAY   => "Towarzyskie wtorki - zamów duży makaron, a drugi dostaniesz za pół ceny."
-      case Calendar.WEDNESDAY => "Spragnione środy - do każdego zamówienia napój z rabatem -50%."
-      case Calendar.THURSDAY  => "Indyjskie czwartki - wszystkie dania kuchni indyjskiej w cenie 15zł."
-      case Calendar.FRIDAY    => "Wege piątek - dania wegetariańskie 15% taniej."
+      case Calendar.MONDAY    => "Towarzyskie poniedziałki - zamów duży makaron, a drugi dostaniesz za pół ceny"
+      case Calendar.TUESDAY   => "Studenckie wtorki - dla studentów wszystkie dania kuchni indyjskiej za 9.90 zł"
+      case Calendar.WEDNESDAY => "Orzeźwiające środy - do każdego Makaruna i Hindusa napój z rabatem -50%."
+      case Calendar.THURSDAY  => "Klasyczne czwartki - duży makaron Bolognese lub Currygodny w cenie małego"
+      case Calendar.FRIDAY    => "Wege piątek - makarony wegetariańskie 15% taniej."
       case _                  => "Dzisiaj jest weekend"
     }
     menu.addToCategory(category, List(todayPromo))
